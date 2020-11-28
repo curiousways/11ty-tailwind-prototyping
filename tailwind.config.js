4d258e9740f9e326487d78a5f607c8ff410a64f6
David Flindall <david@curiousways.com>
2020-11-28 19:32:54 +0000

    site border
    Signed-off-by: David Flindall <david@curiousways.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+     colors: {
+        'idg-blue': '#003667'
+      }
+    },
   },
   variants: {
     extend: {},
