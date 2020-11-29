573c77e27be633ce5facf15983e87d188a346a3b
David Flindall <david@curiousways.com>
2020-11-29 10:58:07 +0000

    preview panels in place
    Signed-off-by: David Flindall <david@curiousways.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,17 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
      colors: {
-        'idg-blue': '#003667'
-      }
+       'idg-black': '#424242',
+       'idg-blue': '#003667',
+      },
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
     },
   },
   variants: {
