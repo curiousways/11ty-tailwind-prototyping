12abbe177ab4b1c235833f6024349ec8103b032c
David Flindall <david@curiousways.com>
2020-11-29 17:30:07 +0000

    final pages
    Signed-off-by: David Flindall <david@curiousways.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,12 @@ module.exports = {
   theme: {
     extend: {
      colors: {
+       'idg-grey-light': '#E5E5E5',
+       'idg-blue-light': '#E4EBF0',
+       'idg-blue-mid': '#B0C4D2',
        'idg-black': '#424242',
        'idg-blue': '#003667',
+       'idg-green': '#BED582',
       },
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
